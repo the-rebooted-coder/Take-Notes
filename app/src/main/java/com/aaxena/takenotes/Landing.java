e48fed7e7b71471df9c3f88399655cf2098bf9a1
Spandan Saxena <spandansaxena@gmail.com>
2020-09-11 01:22:16 +0530

    Linked PlugOn to the App

--- app/src/main/java/com/aaxena/takenotes/Landing.java
+++ app/src/main/java/com/aaxena/takenotes/Landing.java
@@ -2,13 +2,45 @@ package com.aaxena.takenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Landing extends AppCompatActivity {
-
+    private WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        webview = findViewById(R.id.takenotes_plugin);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+        });
+        webview.loadUrl("http://the-rebooted-coder.github.io/Take-Notes/");
+        webview.measure(100, 100);
+        webview.getSettings().setUseWideViewPort(true);
+        webview.getSettings().setLoadWithOverviewMode(true);
+        webview.setVerticalScrollBarEnabled(false);
+        webview.setHorizontalScrollBarEnabled(false);
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webview.canGoBack()) {
+                    webview.goBack();
+                } else {
+                    finish();
+                }
+                return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
