3b13c8809cc877990f157e06e8a01386d97ae9b4
Spandan Saxena <spandansaxena@gmail.com>
2020-09-10 14:59:58 +0530

    Added Vibrate Permission

--- app/src/main/java/com/aaxena/takenotes/SplashScreen.java
+++ app/src/main/java/com/aaxena/takenotes/SplashScreen.java
@@ -2,7 +2,11 @@ package com.aaxena.takenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        //Initiating Flash Screen
+        fireSplashScreen();
+    }
+    private void fireSplashScreen() {
+        int splash_screen_time_out = 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(30);
+                Intent i=new Intent(SplashScreen.this,DeployedChooser.class);
+                startActivity(i);
+                finish();
+            }
+        }, splash_screen_time_out);
     }
 }
