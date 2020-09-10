40632aeef95ff64d996004c2099dbf6fcec374ab
Spandan Saxena <spandansaxena@gmail.com>
2020-09-11 01:25:08 +0530

    Added No Internet Activity

---
+++ app/src/main/java/com/aaxena/takenotes/NoInternet.java
@@ -0,0 +1,14 @@
+package com.aaxena.takenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoInternet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_internet);
+    }
+}
