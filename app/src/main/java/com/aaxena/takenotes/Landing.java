7f3b8b7555ee9a537d8aa4aa3ef972139887abbd
Spandan Saxena <spandansaxena@gmail.com>
2020-09-11 17:58:15 +0530

    More Changes

--- app/src/main/java/com/aaxena/takenotes/Landing.java
+++ app/src/main/java/com/aaxena/takenotes/Landing.java
@@ -18,7 +18,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.browser.customtabs.CustomTabsIntent;
 
 public class Landing extends AppCompatActivity {
-    private WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,38 +26,14 @@ public class Landing extends AppCompatActivity {
 
         //Runtime External storage permission for saving download files
         checkPerms();
+        openCustomTab();
+    }
 
+    private void openCustomTab() {
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
         CustomTabsIntent customTabsIntent = builder.build();
         customTabsIntent.launchUrl(this, Uri.parse("https://the-rebooted-coder.github.io/Take-Notes/"));
-        /*
-        webview = findViewById(R.id.takenotes_plugin);
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setDomStorageEnabled(true);
-        webview.setWebViewClient(new WebViewClient());
-        registerForContextMenu(webview);
-        webview.loadUrl("http://the-rebooted-coder.github.io/Take-Notes/");
-        webview.getSettings().setUseWideViewPort(true);
-        webview.getSettings().setLoadWithOverviewMode(true);
-        webview.setVerticalScrollBarEnabled(false);
-        webview.setHorizontalScrollBarEnabled(false);
-        //handle downloading
-
-
-        webview.setDownloadListener(new DownloadListener()
-        {
-            @Override
-            public void onDownloadStart(String url, String userAgent,
-                                        String contentDisposition, String mimeType,
-                                        long contentLength) {
-                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                v.vibrate(30);
-
-                String uriString =url;
-
-            }}); */
     }
-
 
 
     private void checkPerms() {
@@ -72,60 +47,4 @@ public class Landing extends AppCompatActivity {
         }
     }
 
-    // LONG PRESS
-    @Override
-    public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo){
-        super.onCreateContextMenu(contextMenu, view, contextMenuInfo);
-
-        final WebView.HitTestResult webViewHitTestResult = webview.getHitTestResult();
-
-        if (webViewHitTestResult.getType() == WebView.HitTestResult.IMAGE_TYPE ||
-                webViewHitTestResult.getType() == WebView.HitTestResult.SRC_ANCHOR_TYPE ||
-                webViewHitTestResult.getType() == webViewHitTestResult.SRC_IMAGE_ANCHOR_TYPE ||
-                webViewHitTestResult.getType() == webViewHitTestResult.UNKNOWN_TYPE) {
-
-            contextMenu.setHeaderTitle("Download Image From Below");
-
-            contextMenu.add(0, 1, 0, "Save - Download Image")
-                    .setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-                        @Override
-                        public boolean onMenuItemClick(MenuItem menuItem) {
-
-                            String DownloadImageURL = webViewHitTestResult.getExtra();
-
-
-                                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(DownloadImageURL));
-                                request.allowScanningByMediaScanner();
-                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                                request.setDestinationInExternalPublicDir(
-                                        Environment.DIRECTORY_DOWNLOADS,    //Download folder
-                                        "download");                        //Name of file
-
-                                DownloadManager dm = (DownloadManager) getSystemService(
-                                        DOWNLOAD_SERVICE);
-
-                                dm.enqueue(request);
-                                Toast.makeText(Landing.this,"Image Downloaded Successfully.",Toast.LENGTH_LONG).show();
-                            return false;
-                        }
-                    });
-        }
-    }
-
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            if (keyCode == KeyEvent.KEYCODE_BACK) {
-                if (webview.canGoBack()) {
-                    webview.goBack();
-                } else {
-                    finish();
-                }
-                return true;
-            }
-
-        }
-        return super.onKeyDown(keyCode, event);
-    }
 }
