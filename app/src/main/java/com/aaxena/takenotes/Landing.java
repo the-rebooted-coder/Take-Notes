9d540c9cd84a32cffe6e39a701b5ae49cec5b813
shrish-sharma-git <55692814+shrishsharma69@users.noreply.github.com>
2020-09-11 19:32:12 +0530

    Download as Image Bug Fixed!

--- app/src/main/java/com/aaxena/takenotes/Landing.java
+++ app/src/main/java/com/aaxena/takenotes/Landing.java
@@ -1,25 +1,52 @@
 package com.aaxena.takenotes;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.core.app.NotificationCompat;
+
 import android.Manifest;
 import android.app.DownloadManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Vibrator;
+import android.util.Base64;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.SslErrorHandler;
+import android.webkit.URLUtil;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.browser.customtabs.CustomTabsIntent;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 
 public class Landing extends AppCompatActivity {
+    private WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,22 +54,6 @@ public class Landing extends AppCompatActivity {
 
 
         //Runtime External storage permission for saving download files
-        checkPerms();
-        openCustomTab();
-    }
-
-    private void openCustomTab() {
-
-        Uri uri = Uri.parse("https://the-rebooted-coder.github.io/Take-Notes/");
-        CustomTabsIntent.Builder intentBuilder = new CustomTabsIntent.Builder();
-        intentBuilder.setToolbarColor(Color.BLACK);
-        intentBuilder.setShowTitle(true);
-        intentBuilder.build().launchUrl(this, uri);
-
-    }
-
-
-    private void checkPerms() {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     == PackageManager.PERMISSION_DENIED) {
@@ -51,6 +62,160 @@ public class Landing extends AppCompatActivity {
                 requestPermissions(permissions, 1);
             }
         }
+        webview = findViewById(R.id.takenotes_plugin);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        registerForContextMenu(webview);
+        webview.loadUrl("http://the-rebooted-coder.github.io/Take-Notes/");
+        webview.getSettings().setUseWideViewPort(true);
+        webview.getSettings().setLoadWithOverviewMode(true);
+        webview.setVerticalScrollBarEnabled(false);
+        webview.setHorizontalScrollBarEnabled(false);
+        //handle downloading
+
+
+        webview.setDownloadListener(new DownloadListener()
+        {
+            @Override
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimeType,
+                                        long contentLength) {
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(30);
+                if (url.startsWith("data:")) {  //when url is base64 encoded data
+                    String path = createAndSaveFileFromBase64Url(url);
+                    return;
+                }
+
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setMimeType(mimeType);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie", cookies);
+                request.addRequestHeader("User-Agent", userAgent);
+                request.setDescription(getResources().getString(R.string.msg_downloading));
+                String filename = URLUtil.guessFileName(url, contentDisposition, mimeType);
+                request.setTitle(filename);
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), R.string.msg_downloading, Toast.LENGTH_LONG).show();
+            }});
     }
 
+    public String createAndSaveFileFromBase64Url(String url) {
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        String filetype = url.substring(url.indexOf("/") + 1, url.indexOf(";"));
+        String filename = System.currentTimeMillis() + "." + filetype;
+        File file = new File(path, filename);
+        try {
+            if(!path.exists())
+                path.mkdirs();
+            if(!file.exists())
+                file.createNewFile();
+
+            String base64EncodedString = url.substring(url.indexOf(",") + 1);
+            byte[] decodedBytes = Base64.decode(base64EncodedString, Base64.DEFAULT);
+            OutputStream os = new FileOutputStream(file);
+            os.write(decodedBytes);
+            os.close();
+
+            //Tell the media scanner about the new file so that it is immediately available to the user.
+            MediaScannerConnection.scanFile(this,
+                    new String[]{file.toString()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.i("ExternalStorage", "Scanned " + path + ":");
+                            Log.i("ExternalStorage", "-> uri=" + uri);
+                        }
+                    });
+
+            //Set notification after download complete and add "click to view" action to that
+            String mimetype = url.substring(url.indexOf(":") + 1, url.indexOf("/"));
+            Intent intent = new Intent();
+            intent.setAction(android.content.Intent.ACTION_VIEW);
+            intent.setDataAndType(Uri.fromFile(file), (mimetype + "/*"));
+            PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentText(getString(R.string.msg_file_downloaded))
+                    .setContentTitle(filename)
+                    .setContentIntent(pIntent)
+                    .build();
+
+            notification.flags |= Notification.FLAG_AUTO_CANCEL;
+            int notificationId = 85851;
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(notificationId, notification);
+        } catch (IOException e) {
+            Log.w("ExternalStorage", "Error writing " + file, e);
+            Toast.makeText(getApplicationContext(), R.string.error_downloading, Toast.LENGTH_LONG).show();
+        }
+
+        return file.toString();
+    }
+
+    // LONG PRESS
+    @Override
+    public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo){
+        super.onCreateContextMenu(contextMenu, view, contextMenuInfo);
+
+        final WebView.HitTestResult webViewHitTestResult = webview.getHitTestResult();
+
+        if (webViewHitTestResult.getType() == WebView.HitTestResult.IMAGE_TYPE ||
+                webViewHitTestResult.getType() == WebView.HitTestResult.SRC_ANCHOR_TYPE ||
+                webViewHitTestResult.getType() == webViewHitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
+
+            contextMenu.setHeaderTitle("Download Image From Below");
+
+            contextMenu.add(0, 1, 0, "Save - Download Image")
+                    .setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+
+                            String DownloadImageURL = webViewHitTestResult.getExtra();
+
+                            if(URLUtil.isValidUrl(DownloadImageURL)){
+
+                                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(DownloadImageURL));
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                request.setDestinationInExternalPublicDir(
+                                        Environment.DIRECTORY_DOWNLOADS,    //Download folder
+                                        "download");                        //Name of file
+
+                                DownloadManager dm = (DownloadManager) getSystemService(
+                                        DOWNLOAD_SERVICE);
+
+                                dm.enqueue(request);
+                                Toast.makeText(Landing.this,"Image Downloaded Successfully.",Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                Toast.makeText(Landing.this,"Sorry.. Something Went Wrong.",Toast.LENGTH_LONG).show();
+                            }
+                            return false;
+                        }
+                    });
+        }
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webview.canGoBack()) {
+                    webview.goBack();
+                } else {
+                    finish();
+                }
+                return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
