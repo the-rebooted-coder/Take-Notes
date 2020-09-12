658a32c49ec228e3764a33a2ef56d357744f1087
Spandan Saxena <spandansaxena@gmail.com>
2020-09-13 03:21:59 +0530

    Added Folder Creation Capabilities to the app
    The Newly saved Images will now be stored under Take Notes Dir under Documents

--- app/src/main/java/com/aaxena/takenotes/Landing.java
+++ app/src/main/java/com/aaxena/takenotes/Landing.java
@@ -127,7 +127,7 @@ public class Landing extends AppCompatActivity {
     }
 
     public String createAndSaveFileFromBase64Url(String url) {
-        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS+"/Take Notes");
         String filetype = url.substring(url.indexOf("/") + 1, url.indexOf(";"));
         String filename = "Take Notes "+System.currentTimeMillis() + "." + filetype;
         Toast.makeText(this, R.string.success_toast,Toast.LENGTH_LONG).show();
