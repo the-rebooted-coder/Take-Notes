ad4836a8ecbc04731235c71754bcb239107f59c3
shrish-sharma-git <55692814+shrishsharma69@users.noreply.github.com>
2020-09-18 15:58:43 +0530

    Update vibrate.js

--- docs/js/vibrate.js
+++ docs/js/vibrate.js
@@ -1,4 +1,25 @@
+// Add a global error event listener early on in the page load, to help ensure that browsers
+      // which don't support specific functionality still end up displaying a meaningful message.
+      window.addEventListener('error', function(error) {
+        if (ChromeSamples && ChromeSamples.setStatus) {
+          console.error(error);
+          ChromeSamples.setStatus(error.message + ' (Your browser may not support this feature.)');
+          error.preventDefault();
+        }
+      });
+
 function example1() {
     // Vibrate for 500ms
     navigator.vibrate([500]);
   }
+
+ /* jshint ignore:start */
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-53563471-1', 'auto');
+ga('send', 'pageview');
+/* jshint ignore:end */
+
+ 
