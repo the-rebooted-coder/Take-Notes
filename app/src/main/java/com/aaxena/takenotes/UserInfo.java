5ccd022761182ddc3c71ac1a1ee370bd897de758
shrish-sharma-git <55692814+shrishsharma69@users.noreply.github.com>
2020-09-21 13:10:59 +0530

    UserInfo Activity

---
+++ app/src/main/java/com/aaxena/takenotes/UserInfo.java
@@ -0,0 +1,14 @@
+package com.aaxena.takenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
