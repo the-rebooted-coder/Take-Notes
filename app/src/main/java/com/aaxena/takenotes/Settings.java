24aca9020353056572ebd26f97e05d54c56b96d9
Spandan Saxena <spandansaxena@gmail.com>
2020-11-20 00:31:54 +0530

    Added Tutorial Inside Settings Activity

--- app/src/main/java/com/aaxena/takenotes/Settings.java
+++ app/src/main/java/com/aaxena/takenotes/Settings.java
@@ -2,6 +2,8 @@ package com.aaxena.takenotes;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
 
 public class Settings extends AppCompatActivity {
 
@@ -16,65 +19,58 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
         init();
-
     }
 
     private void init() {
-
-
         Button profile = findViewById(R.id.myacc);
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                v2.vibrate(25);
-                Intent i=new Intent(Settings.this,UserInfo.class);
-                startActivity(i);
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                finish();
-            }
+        profile.setOnClickListener(v -> {
+            Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v2.vibrate(25);
+            Intent i=new Intent(Settings.this,UserInfo.class);
+            startActivity(i);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
         });
 
         Button request = findViewById(R.id.request);
-        request.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                v2.vibrate(25);
-                Intent i=new Intent(Settings.this,feature.class);
-                startActivity(i);
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                finish();
-            }
+        request.setOnClickListener(v -> {
+            Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v2.vibrate(25);
+            Intent i=new Intent(Settings.this,feature.class);
+            startActivity(i);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
         });
 
         Button devs = findViewById(R.id.devs);
-        devs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                v2.vibrate(25);
-                Toast.makeText(Settings.this,"Tip: Tap on our PFP's to reveal more!",Toast.LENGTH_LONG).show();
-                Intent i=new Intent(Settings.this,MeetDevs.class);
-                startActivity(i);
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                finish();
-            }
+        devs.setOnClickListener(v -> {
+            Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v2.vibrate(25);
+            Toast.makeText(Settings.this,"Tip: Tap on our PFP's to reveal more!",Toast.LENGTH_LONG).show();
+            Intent i=new Intent(Settings.this,MeetDevs.class);
+            startActivity(i);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
         });
 
         Button privacy = findViewById(R.id.privacy);
-        privacy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                v2.vibrate(25);
-                Intent i=new Intent(Settings.this,PrivacyPolicy.class);
-                startActivity(i);
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                finish();
-            }
+        privacy.setOnClickListener(v -> {
+            Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v2.vibrate(25);
+            Intent i=new Intent(Settings.this,PrivacyPolicy.class);
+            startActivity(i);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        });
+
+        Button tutorial = findViewById(R.id.tutorial);
+        tutorial.setOnClickListener(v -> {
+            String url ="https://the-rebooted-coder.github.io/Take-Notes-Web/tutorial";
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            builder.setToolbarColor(Color.parseColor("#006400"));
+            CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.launchUrl(Settings.this, Uri.parse(url));
         });
 
     }
