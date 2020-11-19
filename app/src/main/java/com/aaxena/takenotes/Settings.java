b9d17e3ca7cb2746e748e5e7b8de142036a46225
Spandan Saxena <spandansaxena@gmail.com>
2020-11-20 01:16:35 +0530

    Optimized Imports
    Removed ic_done vector

--- app/src/main/java/com/aaxena/takenotes/Settings.java
+++ app/src/main/java/com/aaxena/takenotes/Settings.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
--- app/src/main/java/com/aaxena/takenotes/UserInfo.java
+++ app/src/main/java/com/aaxena/takenotes/UserInfo.java
@@ -7,7 +7,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
