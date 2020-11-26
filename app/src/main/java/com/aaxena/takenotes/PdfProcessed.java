d67d991e0317611a0e6c2b6bcc90bb46b2a8d5fb
Spandan Saxena <spandansaxena@gmail.com>
2020-11-26 17:43:40 +0530

    PDF Saved Activity now auto-quits in 2secs after the shot
    Much better optimisation

--- app/src/main/java/com/aaxena/takenotes/PdfProcessed.java
+++ app/src/main/java/com/aaxena/takenotes/PdfProcessed.java
@@ -2,6 +2,7 @@ package com.aaxena.takenotes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,14 @@ public class PdfProcessed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_processed);
+
+        int time_out = 1900;
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(PdfProcessed.this,Landing.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        },time_out);
     }
 
     @Override
