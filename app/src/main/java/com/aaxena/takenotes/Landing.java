4d41a6234768b357224f5a35bea7f5784dc003e3
Spandan Saxena <spandansaxena@gmail.com>
2021-01-17 22:06:15 +0530

    Removed OnBackPress due to Error

--- app/src/main/java/com/aaxena/takenotes/Landing.java
+++ app/src/main/java/com/aaxena/takenotes/Landing.java
@@ -82,7 +82,6 @@ public class Landing extends AppCompatActivity {
     private AppUpdateManager mAppUpdateManager;
     private static final int RC_APP_UPDATE = 11;
     FirebaseAuth mAuth;
-    private long pressedTime;
     private String hello;
 
     @Override
@@ -554,14 +553,5 @@ public class Landing extends AppCompatActivity {
             finish();
         }
     }
-    @Override
-    public void onBackPressed() {
-        if (pressedTime + 2000 > System.currentTimeMillis()) {
-            super.onBackPressed();
-            finish();
-        } else {
-            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
-        }
-        pressedTime = System.currentTimeMillis();
-    }
+
 }
