8cb152ba2a351d2d68049d81dab62832ff3ba3e3
Spandan Saxena <spandansaxena@gmail.com>
2021-06-05 00:36:14 +0530

    Removed PDF Welcome Slide

--- app/src/main/java/com/aaxena/takenotes/WelcomeActivity.java
+++ app/src/main/java/com/aaxena/takenotes/WelcomeActivity.java
@@ -51,9 +51,6 @@ public class WelcomeActivity extends AppCompatActivity {
         dotsLayout = findViewById(R.id.layoutDots);
         btnSkip = findViewById(R.id.btn_skip);
         btnNext = findViewById(R.id.btn_next);
-
-        // layouts of all welcome sliders
-        // add few more layouts if you want
         layouts = new int[]{
                 R.layout.welcome_side1,
                 R.layout.welcome_side2,
@@ -61,7 +58,6 @@ public class WelcomeActivity extends AppCompatActivity {
                 R.layout.welcome_side5,
                 R.layout.welcome_side6,
                 R.layout.welcome_side7,
-                R.layout.welcome_side9,
                 R.layout.welcome_side10,
                 R.layout.welcome_all_set,
                 R.layout.welcome_side8,
