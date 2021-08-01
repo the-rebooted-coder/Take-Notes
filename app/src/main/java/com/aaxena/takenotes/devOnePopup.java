954bdb1ae7a62f0febcc90882f810571c9650bfb
Spandan Saxena <spandansaxena@gmail.com>
2021-08-01 21:08:37 +0530

    Made devOne dynamic

--- app/src/main/java/com/aaxena/takenotes/devOnePopup.java
+++ app/src/main/java/com/aaxena/takenotes/devOnePopup.java
@@ -7,9 +7,18 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class devOnePopup extends AppCompatActivity {
 
@@ -17,6 +26,7 @@ public class devOnePopup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dev_one_popup);
+        ArrayList<String> urls= new ArrayList<>();
 
         ImageView devOneGh = findViewById(R.id.devOneGh);
         devOneGh.setOnClickListener(view -> {
@@ -37,12 +47,39 @@ public class devOnePopup extends AppCompatActivity {
             startActivity(intent);
         });
         ImageView devOneWeb = findViewById(R.id.devOneWeb);
+        new Thread(() -> {
+            try {
+                // Create a URL for the desired page
+                URL url = new URL("https://raw.githubusercontent.com/the-rebooted-coder/Spandan-Saxena-Portfolio/master/porfolioLink.txt");
+                //First open the connection
+                HttpURLConnection conn=(HttpURLConnection) url.openConnection();
+                conn.setConnectTimeout(60000); // timing out in a minute
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String str;
+                while ((str = in.readLine()) != null) {
+                    urls.add(str);
+                }
+                in.close();
+            } catch (Exception e) {
+                Log.d("MyTag",e.toString());
+            }
+            try {
+                this.runOnUiThread(() -> {
+                    if (!urls.isEmpty()) {
+                    }
+                });
+            }
+            catch (NullPointerException e){
+                //Very Important
+                //DO NOTE REMOVE THIS
+            }
+        }).start();
         devOneWeb.setOnClickListener(view -> {
             vibrateDevice();
             Intent intent = new Intent();
             intent.setAction(Intent.ACTION_VIEW);
             intent.addCategory(Intent.CATEGORY_BROWSABLE);
-            intent.setData(Uri.parse("https://spandansaxena.codes"));
+            intent.setData(Uri.parse(urls.toString().replaceAll("\\[", "").replaceAll("]","")));
             startActivity(intent);
         });
         ImageView devOneMail = findViewById(R.id.devOneMail);
