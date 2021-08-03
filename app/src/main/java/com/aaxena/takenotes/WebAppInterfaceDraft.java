befb32e69d18aee901782969db76272371efb6e8
Spandan Saxena <spandansaxena@gmail.com>
2021-08-03 18:01:52 +0530

    Removed Draft DB

--- app/src/main/java/com/aaxena/takenotes/WebAppInterfaceDraft.java
+++ app/src/main/java/com/aaxena/takenotes/WebAppInterfaceDraft.java
@@ -5,7 +5,6 @@ import android.webkit.JavascriptInterface;
 
 public class WebAppInterfaceDraft {
     private Context context;
-    private DraftDB draftDB;
 
     public WebAppInterfaceDraft(Context context)
     {
@@ -15,8 +14,6 @@ public class WebAppInterfaceDraft {
     public void saveDraft(String draft)
     {
         if (!draft.isEmpty()) {
-            draftDB = new DraftDB(context);
-            draftDB.addNewCourse(draft);
         }
     }
 }
