bf3ccde2cc32eafbb85f5317dbcd3f02f004e3dc
Shrish Sharma <shrish0608@gmail.com>
2021-10-17 12:58:52 +0530

    Create keyCode.sh

---
+++ docs/hack/keyCode.sh
@@ -0,0 +1,5 @@
+2AAE6C35C94FCFB415DBE95F408B9CE91EE846ED
+
+keytool -list -v -keystore ~/.android/debug.keystore -alias androiddebugkey -storepass android -keypass android 
+
+2AAE6C35C94FCFB415DBE95F408B9CE91EE846ED
